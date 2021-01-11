6483161bd5dafbe89ad7e5a44ddcfddc75c17dc7
James Clarke <jkc200013@gmail.com>
2021-01-11 18:47:58 +0000

    working CMakeList.txt!!!

--- scripts/ninjabuild.sh
+++ scripts/ninjabuild.sh
@@ -1,2 +1,2 @@
-cmake -B ../build -G Ninja -DLLVM_DIR="C:/Program Files (x86)/llvm" -DCMAKE_C_COMPILER=clang -DCMAKE_CXX_COMPILER=clang++ -DCMAKE_BUILD_TYPE=Release
+cmake -B ../build -G Ninja -LLVM_PATH="C:/Program Files (x86)/llvm" -DCMAKE_C_COMPILER=clang -DCMAKE_CXX_COMPILER=clang++ -DCMAKE_BUILD_TYPE=Release
 ninja -C ../build -j 16
--- scripts/unixbuild.sh
+++ scripts/unixbuild.sh
@@ -1,4 +1,4 @@
-cmake -B ../build -G "Unix Makefiles" -DLLVM_DIR="C:/Program Files (x86)/llvm" -DCMAKE_C_COMPILER=clang -DCMAKE_CXX_COMPILER=clang++ -DCMAKE_CXX_FLAGS=-Wall -DCMAKE_BUILD_TYPE=Release
+cmake -B ../build -G "Unix Makefiles" -DLLVM_PATH="C:/Program Files (x86)/llvm" -DCMAKE_C_COMPILER=clang -DCMAKE_CXX_COMPILER=clang++ -DCMAKE_CXX_FLAGS=-Wall -DCMAKE_BUILD_TYPE=Release
 cd ../build
 make clean
 make -j 16
